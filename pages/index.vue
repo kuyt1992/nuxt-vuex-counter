c8708742929cc9b3b8e85590c944b725ca8dbc07
kuyt1992 <kuyt1992@gmail.com>
2021-09-12 16:14:05 +0900

    feat: Nuxt&Vuexでカウンターテストアプリ作成

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="container">
+    <div>
+      <Counter/>
+      <hr>
+      <Counter/>
+      <hr>
+      <Counter/>
+      <hr>
+    </div>
+  </section>
+</template>
+
+<script>
+import Counter from '~/components/Counter.vue';
+
+export default {
+  components: {
+    Counter
+  }
+}
+</script>
